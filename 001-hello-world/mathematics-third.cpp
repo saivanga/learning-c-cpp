2fa331e8552db7aafe8307b1ad5135d670dd0863
Sai Vanga <sai@sai.rocks>
2024-08-19 12:26:32 -0700

    made circle area calculator

--- 001-hello-world/mathematics-third.cpp
+++ 001-hello-world/mathematics-third.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {
+    const double pi = 3.1415926; 
+    double radius;
+    cout << "What is the radius of the circle: ";
+    cin >> radius;
+
+    const double area = pow(radius, 2) * pi;
+
+    cout << area << endl;
 
     return 0;
 }
